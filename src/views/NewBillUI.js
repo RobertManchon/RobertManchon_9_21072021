cfc1a88ff129d633f220b7e8c4af057cb0da3827
RManchon <erasme666@gmail.com>
2021-08-04 10:42:32 +0200

    All bugs fixed - V1

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -28,6 +28,7 @@ export default () => {
                   <div class="col-half">
                     <label for="expense-name" class="bold-label">Nom de la dépense</label>
                     <input type="text" class="form-control blue-border" data-testid="expense-name" placeholder="Vol Paris Londres" />
+                    <div class="hideErrorMessage" id="errorExpenseName" data-testid="errorExpenseName">Vous devez entrer au moins 5 caractères.</div>
                   </div>
                   <div class="col-half">
                     <label for="datepicker" class="bold-label">Date</label>
@@ -56,8 +57,8 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
-                    <div class="error error-imageFormat">Votre justificatif doit être une image (JPG ou PNG)</div>
-                    </div>
+                    <div class="hideErrorMessage" id="errorFileType" data-testid="errorFile">Le justificatif à fournir doit être au format "jpg", "jpeg" ou "png".</div>
+                  </div>
                 </div>
             </div>
             <div class="row">
